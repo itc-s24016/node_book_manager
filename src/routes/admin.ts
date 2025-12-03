174bec688a8e1cf66e8035ea91c612eeb22f20ad
itc-s24016 <s24016@std.it-college.ac.jp>
2025-12-03 11:17:39 +0900

    書籍登録まで

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -185,4 +185,74 @@ router.delete('/publisher',
     }
   });
 
+//書籍登録
+router.post('/book',
+  check('isbn').notEmpty().withMessage('書籍IDは必須です'),
+  check('title').notEmpty().withMessage('書籍名は必須です'),
+  check('authorId').notEmpty().withMessage('著者IDは必須です'),
+  check('publisherId').notEmpty().withMessage('出版社IDは必須です'),
+  check('publicationYear').notEmpty().withMessage('出版年は必須です'),
+  check('publicationMonth').notEmpty().withMessage('出版月は必須です'),
+  async (req, res) => {
+
+    const result = validationResult(req)
+    if (!result.isEmpty()) {
+      const firstError = result.array()[0]
+      return res.status(400).json({ message: firstError.msg })
+    }
+
+    try {
+      // isbnの重複確認
+      const existingBook = await prisma.book.findUnique({
+        where: {
+          isbn: req.body.isbn
+        }
+      })
+      if (existingBook) {
+        return res.status(400).json({ message: '既に登録されている書籍IDです' })
+      }
+
+      // autherIdが一致する著者のisDeletedがfalseであることを確認
+      const author = await prisma.author.findFirst({
+        where: {
+          id: req.body.authorId,
+          isDeleted: false
+        }
+      })
+      if (!author) {
+        return res.status(400).json({ message: '無効な著者IDです' })
+      }
+
+      // publisherIdが一致する出版社のisDeletedがfalseであることを確認
+      const publisher = await prisma.publisher.findFirst({
+        where: {
+          id: req.body.publisherId,
+          isDeleted: false
+        }
+      })
+      if (!publisher) {
+        return res.status(400).json({ message: '無効な出版社IDです' })
+      }
+
+      await prisma.book.create({
+        data: {
+          isbn: req.body.isbn,
+          title: req.body.title,
+          authorId: req.body.authorId,
+          publisherId: req.body.publisherId,
+          publicationYear: req.body.publicationYear,
+          publicationMonth: req.body.publicationMonth
+        }
+      })
+
+      return res.status(200).json({
+        message: '登録しました'
+      })
+
+    } catch (e: any) {
+      return res.status(500).json({ message: 'サーバーエラーが発生しました' })
+    }
+  }
+)
+
 export default router
